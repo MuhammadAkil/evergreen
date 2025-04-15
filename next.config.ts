09a0205519940c5c27f8ea77a29ea3e3e4579a6e
Muhammad  Akil <64638482+MuhammadAkil@users.noreply.github.com>
2025-04-15 23:36:57 +0500

    disable eslint warnings

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config: any) => {
+    config.module.rules.push({
+      test: /\.(png|jpg|jpeg|gif|svg|eot|ttf|woff|woff2)$/,
+      type: 'asset/resource',
+    });
+    return config;
+  },
 
 };
 
